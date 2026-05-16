363031fbe1084e85095fb41452e16cad327d6f81
kate123-cmd <kashasingh22@gmail.com>
2026-05-16 08:51:16 -0400

    Error handling

--- server.py
+++ server.py
@@ -11,28 +11,31 @@ app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
-    # Retrieve the text to analyze from the request arguments 
+    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the sentiment_analyzer function and store the response 
+    # Pass the text to the sentiment_analyzer function and store the response
     response = sentiment_analyzer(text_to_analyze)
 
-    # Extract the label and score from the response 
-    label = response['label'] 
+    # Extract the label and score from the response
+    label = response['label']
     score = response['score']
 
-    # Check if the label is None, indicating an error or invalid input
-	if label is None:
-		return "Invalid input! Try again."
-	else:
-        # Return a formatted string with the sentiment label and score 
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    # Check if the label is None,
+    # indicating an error or invalid input
+    if label is None:
 
+        return "Invalid input! Try again."
+
+    else:
+
+        # Return a formatted string
+        # with the sentiment label and score
+        return (
+            "The given text has been identified as {} "
+            "with a score of {}."
+        ).format(label.split('_')[1], score)
+    
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
